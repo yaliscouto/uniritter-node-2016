74022e52e0fd08e9318fcd8e023cb8bbba348855
dclucas <diogo.lucas@gmail.com>
2016-05-28 18:44:18 -0300

    Introduces istanbul for code coverage.

--- features/step_definitions/notes.js
+++ features/step_definitions/notes.js
@@ -7,17 +7,4 @@ module.exports = function () {
     this.Given(/^a valid note payload$/, function () {
         this.fixture = require('../fixtures/valid-note')
        });
-    
-    this.When(/^I (\w+) it against the \/(.*) endpoint$/, function (verb, endpoint) {
-        var that = this;
-        return this.doHttpRequest(endpoint, verb, that.fixture.request)
-        .spread(function (response) {
-            that.response = response;
-            return response;
-        });
-    });
-    
-    this.Then(/^a payload containing the newly created resource$/, function () {
-        expect(this.response.body).to.containSubset(this.fixture.request);
-    });
 };
