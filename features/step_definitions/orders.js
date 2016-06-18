44d8d53df82cab2c83e2bb10ea5c2b7ff5b5684d
Diogo Lucas <diogo.lucas@gmail.com>
2016-06-18 13:04:16 +0000

    adds order status check tests

---
+++ features/step_definitions/orders.js
@@ -0,0 +1,46 @@
+module.exports = function () {
+    const 
+        _ = require('lodash'),
+        chai = require('chai'),
+        expect = chai.expect;
+        
+    this.Given(/^an existing order with a (.*) status$/, function (status) {
+        const 
+            that = this,
+            payload = {
+            data: {
+                type: 'orders',
+                attributes: {
+                    status: status,
+                    items: [{ product_id: '598b04ea-8c20-4240-9c2b-1d36350a8d33', quantity: 1}]
+                    }
+                }
+            }
+        
+
+        return this.doHttpRequest('orders', 'post', payload)
+        .then((response) => {
+            that.existingOrder = response.body;
+            return response;
+        });
+    });
+    
+    this.When(/^I search this order$/, function () {
+        const 
+            that = this,
+            id = this.existingOrder.data.id;
+        return this.doHttpRequest('orders/' + id, 'get', undefined)
+        .then((response) => {
+            that.responseBody = JSON.parse(response.body);
+            return response;
+        });
+    });
+    
+    this.Then(/^I receive the order data$/, function () {
+        expect(this.responseBody.data).not.to.be.undefined;
+    });
+    
+    this.Then(/^its status is (.*)$/, function (status) {
+        expect(this.responseBody.data.attributes.status).to.equal(status);
+    });
+}
